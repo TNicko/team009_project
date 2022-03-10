6538bcb4c85f8509c3b6110d6d3d4e4995466140
Nickolai Tchesnokov <nickolaitchesnokov@wireless-student-pt8-164-196.lut.ac.uk>
2022-03-10 15:47:10 +0000

    created delete_tables.sql

---
+++ src/db/delete_tables.sql
@@ -0,0 +1,19 @@
+
+/* Deletes all rows from all tables in database. */
+
+DELETE IGNORE FROM ticket;
+DELETE IGNORE FROM ticket_log;
+DELETE IGNORE FROM feedback;
+DELETE IGNORE FROM solution;
+DELETE IGNORE FROM handler;
+DELETE IGNORE FROM employee;
+DELETE IGNORE FROM account;
+DELETE IGNORE FROM ticket_expertise;
+DELETE IGNORE FROM expertise;
+DELETE IGNORE FROM handler_expertise;
+DELETE IGNORE FROM ticket_hardware;
+DELETE IGNORE FROM ticket_software;
+DELETE IGNORE FROM ticket_os;
+DELETE IGNORE FROM hardware;
+DELETE IGNORE FROM sofware;
+DELETE IGNORE FROM os;
