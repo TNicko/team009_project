7f690626c8fa456888bfa88ee39e8e8260f6b091
Nickolai Tchesnokov <nickolaitchesnokov@wireless-student-pt8-161-98.lut.ac.uk>
2022-03-11 14:57:03 +0000

    updated schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -2,6 +2,62 @@
 
 CREATE DATABASE IF NOT EXISTS teamb009;
 
+CREATE TABLE IF NOT EXISTS hardware (
+    hardware_id MEDIUMINT UNSIGNED,
+    name VARCHAR(100),
+    PRIMARY KEY (hardware_id)
+);
+
+CREATE TABLE IF NOT EXISTS software (
+    software_id MEDIUMINT UNSIGNED,
+    name VARCHAR(100),
+    PRIMARY KEY (software_id)
+);
+
+CREATE TABLE IF NOT EXISTS os (
+    os_id MEDIUMINT UNSIGNED,
+    name VARCHAR(100),
+    PRIMARY KEY (os_id)
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    employee_id MEDIUMINT UNSIGNED,
+    name VARCHAR(150),
+    job VARCHAR(100),
+    department VARCHAR(100), -- ??? seperate department table ???
+    telephone CHAR(10),
+    PRIMARY KEY (employee_id)  
+);
+
+CREATE TABLE IF NOT EXISTS handler (
+    handler_id MEDIUMINT UNSIGNED,
+    internal BOOLEAN,
+    PRIMARY KEY (handler_id),  
+    FOREIGN KEY (handler_id) REFERENCES employee(employee_id)
+);
+
+CREATE TABLE IF NOT EXISTS account (
+    user_id MEDIUMINT UNSIGNED,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES employee(employee_id)
+);
+
+CREATE TABLE IF NOT EXISTS expertise (
+    expertise_id MEDIUMINT UNSIGNED,
+    name VARCHAR(100),
+    PRIMARY KEY (expertise_id)
+);
+
+CREATE TABLE IF NOT EXISTS handler_expertise (
+    handler_id MEDIUMINT UNSIGNED,
+    expertise_id MEDIUMINT UNSIGNED,
+    FOREIGN KEY (expertise_id) REFERENCES expertise(expertise_id),
+    FOREIGN KEY (handler_id) REFERENCES handler(handler_id)
+);
+
+
 CREATE TABLE IF NOT EXISTS ticket (
     ticket_id MEDIUMINT UNSIGNED,
     employee_id MEDIUMINT UNSIGNED NOT NULL,
@@ -46,50 +102,12 @@ CREATE TABLE IF NOT EXISTS solution(
     FOREIGN KEY (handler_id) REFERENCES handler(handler_id)
 );
 
-CREATE TABLE IF NOT EXISTS handler (
-    handler_id MEDIUMINT UNSIGNED,
-    internal BOOLEAN,
-    PRIMARY KEY (handler_id),  
-    FOREIGN KEY (handler_id) REFERENCES employee(employee_id)
-);
-
-CREATE TABLE IF NOT EXISTS employee (
-    employee_id MEDIUMINT UNSIGNED,
-    name VARCHAR(150),
-    job VARCHAR(100),
-    department VARCHAR(100), -- ??? seperate department table ???
-    telephone CHAR(10),
-    PRIMARY KEY (employee_id)  
-);
-
-CREATE TABLE IF NOT EXISTS account (
-    user_id MEDIUMINT UNSIGNED,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(50) NOT NULL,
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (user_id) REFERENCES employee(employee_id)
-);
-
 CREATE TABLE IF NOT EXISTS ticket_expertise (
     expertise_id MEDIUMINT UNSIGNED,
     ticket_id MEDIUMINT UNSIGNED,
     FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id),
     FOREIGN KEY (expertise_id) REFERENCES expertise(expertise_id)
 );
-
-CREATE TABLE IF NOT EXISTS expertise (
-    expertise_id MEDIUMINT UNSIGNED,
-    name VARCHAR(100),
-    PRIMARY KEY (expertise_id)
-);
-
-CREATE TABLE IF NOT EXISTS handler_expertise (
-    handler_id MEDIUMINT UNSIGNED,
-    expertise_id MEDIUMINT UNSIGNED,
-    FOREIGN KEY (expertise_id) REFERENCES expertise(expertise_id),
-    FOREIGN KEY (handler_id) REFERENCES handler(handler_id)
-);
-
 
 CREATE TABLE IF NOT EXISTS ticket_hardware (
     hardware_id MEDIUMINT UNSIGNED,
@@ -110,22 +128,4 @@ CREATE TABLE IF NOT EXISTS ticket_os (
     ticket_id MEDIUMINT UNSIGNED,
     FOREIGN KEY (os_id) REFERENCES os(os_id),
     FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id)
-);
-
-CREATE TABLE IF NOT EXISTS hardware (
-    hardware_id MEDIUMINT UNSIGNED,
-    name VARCHAR(100),
-    PRIMARY KEY (hardware_id)
-);
-
-CREATE TABLE IF NOT EXISTS software (
-    software_id MEDIUMINT UNSIGNED,
-    name VARCHAR(100),
-    PRIMARY KEY (software_id)
-);
-
-CREATE TABLE IF NOT EXISTS os (
-    os_id MEDIUMINT UNSIGNED,
-    name VARCHAR(100),
-    PRIMARY KEY (os_id)
 );
