46e05d780629b0ff2a2921803fd31b88493ea4bf
Tangil <21197843+TheBlocks@users.noreply.github.com>
2022-03-11 20:46:02 +0000

    Add Docker to project

--- src/server.js
+++ src/server.js
@@ -12,8 +12,13 @@ app.use(express.static('public'))
 
 // --- sci project connection ---
 const mysql = require('mysql')
+// On sci-project, the MySQL database is located at mysql://localhost
+// On our containerised Docker app, it is located at mysql://mysql
+// We add an environment variable from the Docker Compose configuration
+// called DOCKER so that we can use the appropriate host
+const host = process.env.DOCKER ? "mysql" : "localhost"
 const dbConn = mysql.createConnection({
-    host: 'localhost',
+    host: host,
     user: 'teamb009',
     password: '6TfmSlNpBt',
     database: 'teamb009',
