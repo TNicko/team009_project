8834bc799d7a9200efc586b2ea3337e3a04f60d6
JahanzebAbro <jahanzebabro2001@gmail.com>
2022-03-19 22:42:19 +0000

    added getAllForTicketId function in ticketLogModel

--- src/models/ticketLogModel.js
+++ src/models/ticketLogModel.js
@@ -1,3 +1,22 @@
-class TicketLog{
-    
+class TicketLog {
+    constructor(log_id, ticket_id, update_date, update_type, update_value) {
+        this.log_id = log_id;
+        this.ticket_id = ticket_id;
+        this.update_date = update_date;
+        this.update_type = update_type;
+        this.update_value = update_value;
+    }
+
+    static async getAllForTicketId(conn, id) {
+        let queryString =
+            "SELECT log_id AS id, update_date AS date, update_type AS type, update_value AS value FROM ticket_log WHERE ticket_id = ? ORDER BY update_date DESC";
+        let queryParams = [id];
+
+        let logs = await conn.query(queryString, queryParams);
+
+        return logs.map(
+            log => new TicketLog(log.id, log.date, log.type, log.value)
+        );
+    }
 }
+module.exports = TicketLog;
