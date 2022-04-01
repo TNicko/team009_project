8f455f6c69a6a4e3e61af8a04257a356c8f169a5
Nickolai Tchesnokov <nickolaitchesnokov@Nickolais-MacBook-Air.local>
2022-04-01 17:56:20 +0100

    updated checkbox functionality

--- src/public/scripts/submit_problem.js
+++ src/public/scripts/submit_problem.js
@@ -7,21 +7,23 @@
     // check
     if ($(this).is(':checked')) {
         if (chb_type == "chb_hardware") {
-            $("#hardware_input").show();
+            $('<p>Test</p>').insertAfter("#hardware_list p:first-child");
+            $("#hardware_input").css("display", "flex");
         } 
         else if (chb_type == "chb_software") {
-            $("#software_input").show();
+            $("#software_input").css("display", "flex");
         }
         else if (chb_type == "chb_network") {
         
         }
         else if (chb_type == "chb_other") {
-            $("#other_input").show();
+            $("#other_input").css("display", "flex");
         }
     }
     // uncheck
     if (!$(this).is(':checked')) {
         if (chb_type == "chb_hardware") {
+            
             $("#hardware_input").hide();
         } 
         else if (chb_type == "chb_software") {
