37f6bc89d1fdcd012ccdcde08fddef8cda9009c6
TNicko <nicko.tchesnokov@gmail.com>
2022-04-02 21:46:17 +0100

    updated problem types checkbox functionality in submit_problem.ejs

--- src/public/scripts/submit_problem.js
+++ src/public/scripts/submit_problem.js
@@ -7,7 +7,6 @@
     // check
     if ($(this).is(':checked')) {
         if (chb_type == "chb_hardware") {
-            $('<p>Test</p>').insertAfter("#hardware_list p:first-child");
             $("#hardware_input").css("display", "flex");
         } 
         else if (chb_type == "chb_software") {
@@ -36,7 +35,57 @@
             $("#other_input").hide();
         }
     }
-  });
+});
+
+$('#add_hardware_btn').click(function() {
+    const inputDiv = document.createElement("div");
+    inputDiv.className = "hardware_input_sec";
+    const input = document.createElement("input");
+    input.type = "text";
+    input.className = "pt_input_field";
+    input.list = "hardware_list";
+    input.placeholder = "hardware id";
+    const btnDiv = document.createElement("div");
+    btnDiv.className = "input_del";
+    const btn = document.createElement("button");
+    btn.className = "delete_input_btn";
+    btn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+    // Delete button clicked -> delete input field
+    btn.addEventListener("click", function() {
+        $(this).parent().parent().remove();
+    });
+
+    btnDiv.append(btn);
+    inputDiv.append(input);
+    inputDiv.append(btnDiv);
+
+    $(inputDiv).insertAfter("#hardware_input .hardware_input_sec:last");
+});
+
+$('#add_software_btn').click(function() {
+    const inputDiv = document.createElement("div");
+    inputDiv.className = "software_input_sec";
+    const input = document.createElement("input");
+    input.type = "text";
+    input.className = "pt_input_field";
+    input.list = "software_list";
+    input.placeholder = "software id";
+    const btnDiv = document.createElement("div");
+    btnDiv.className = "input_del";
+    const btn = document.createElement("button");
+    btn.className = "delete_input_btn";
+    btn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+    // Delete button clicked -> delete input field
+    btn.addEventListener("click", function() {
+        $(this).parent().parent().remove();
+    });
+
+    btnDiv.append(btn);
+    inputDiv.append(input);
+    inputDiv.append(btnDiv);
+
+    $(inputDiv).insertAfter("#software_input .software_input_sec:last");
+});
 
 
 $('#submit-problem-btn').click(function() {
