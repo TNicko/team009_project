662982b0c6b39098e5fc2fd70fecb3ab315da29d
Lycrat <Panya_R@outlook.com>
2022-04-05 16:49:40 +0100

    added add, deleteFromTicket, addToTicket and update functionalities

--- src/models/hardwareModel.js
+++ src/models/hardwareModel.js
@@ -23,13 +23,51 @@ class Hardware{
         queryParams.push(skip, limit);
 
         let hardwares = await conn.query(queryString, queryParams);
-
+        
         return hardwares.map(
             hardware => new Hardware(hardware.name, hardware.serial)
         );
-
+        
         
     }
+
+    static async add(conn, serial, name){
+        let queryString = `INSERT INTO hardware (hardware_serial, name)\n VALUES (?, ?)`;
+        await conn.query(queryString, [serial, name]);
+    
+    }
+
+    static async addToTicket(conn, serial, ticket_id){
+        let queryString = `INSERT INTO ticket_hardware (hardware_serial, ticket_id) \n VALUES (?,?)`;
+        await conn.query(queryString, [serial, ticket_id]);
+    }
+
+    static async deleteFromTicket(conn, serial, ticket_id){
+        let queryString = `DELETE FROM ticket_hardware WHERE ticket_id = ? and hardware_serial = ?`;
+        await conn.query(queryString, [ticket_id, serial]);
+    }
+    
+    static async update(conn, serial, newSerial = null, name = null){
+        let queryString = `UPDATE hardware \n SET`;
+        let queryParams = []
+
+        if(newSerial !== null){
+            queryString += ` hardware_serial = ?`;
+            queryParams.push(newSerial)
+        }
+        
+        if(name !== null){
+            queryString += ` name = ?`;
+            queryParams.push(name)
+        }
+
+        queryString += `\n WHERE hardware_serial = ?`;
+        queryParams.push(serial)
+
+        await conn.query(queryString, queryParams)
+
+    }
+
 }
 
 module.exports = Hardware;
