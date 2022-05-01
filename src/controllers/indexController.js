55de34e714498c85ac20dbeda22c7c0f80512046
Tangil <21197843+TheBlocks@users.noreply.github.com>
2022-05-01 17:06:40 +0100

    Format indexController.js

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -9,33 +9,33 @@ router.get('/', checkAuthenticated, async (req, res) => {
     let result = await Ticket.getAll(conn, 0, 100);
 
     let user = await User.getById(conn, req.user.id);
-    
-    if(user.type == 'admin'){
-        res.render('./index/admin', { username: req.user.username});
-    } 
-    if(user.type == 'user'){
-        res.render('./index/user', { username: req.user.username});
-    } 
-    if(user.type == 'specialist'){
-        res.render('./index/specialist', { username: req.user.username});
-    } 
-    if(user.type == 'analyst'){
-        res.render('./index/analyst', { username: req.user.username});
-    } 
+
+    if (user.type == 'admin') {
+        res.render('./index/admin', {username: req.user.username});
+    }
+    if (user.type == 'user') {
+        res.render('./index/user', {username: req.user.username});
+    }
+    if (user.type == 'specialist') {
+        res.render('./index/specialist', {username: req.user.username});
+    }
+    if (user.type == 'analyst') {
+        res.render('./index/analyst', {username: req.user.username});
+    }
 })
 
 // INDEX PAGES
 router.get('/admin', async (req, res) => {
-    res.render('./index/admin', { username: req.user.username});
+    res.render('./index/admin', {username: req.user.username});
 })
 router.get('/analyst', async (req, res) => {
-    res.render('./index/analyst', { username: req.user.username});
+    res.render('./index/analyst', {username: req.user.username});
 })
 router.get('/ext_specialist', async (req, res) => {
-    res.render('./index/ext_specialist', { username: req.user.username});
+    res.render('./index/ext_specialist', {username: req.user.username});
 })
 router.get('/user', async (req, res) => {
-    res.render('./index/user', { username: req.user});
+    res.render('./index/user', {username: req.user});
 })
 
 // Tables
@@ -44,27 +44,27 @@ router.get('/hardware', async (req, res) => {
     res.render('./tables/hardware', {username: req.user.username, hardwares: hardwares});
 })
 router.get('/software', async (req, res) => {
-    res.render('./tables/software', { username: req.user.username});
+    res.render('./tables/software', {username: req.user.username});
 })
 router.get('/os', async (req, res) => {
-    res.render('./tables/os', { username: req.user.username});
+    res.render('./tables/os', {username: req.user.username});
 })
 
 // Other
 router.get('/ticket-information', async (req, res) => {
-    res.render('./ticket-information', { username: req.user.username});
+    res.render('./ticket-information', {username: req.user.username});
 })
 router.get('/account', async (req, res) => {
-    res.render('./account', { username: req.user.username});
+    res.render('./account', {username: req.user.username});
 })
 router.get('/submit_problem', async (req, res) => {
-    res.render('./submit_problem', { username: req.user.username});
+    res.render('./submit_problem', {username: req.user.username});
 })
 router.get('/all_tickets', async (req, res) => {
-    res.render('./submit_problem', { username: req.user.username});
+    res.render('./submit_problem', {username: req.user.username});
 })
 router.get('/users', async (req, res) => {
-    res.render('./users', { username: req.user.username});
+    res.render('./users', {username: req.user.username});
 })
 
 // Redirects to login if not authenticated
