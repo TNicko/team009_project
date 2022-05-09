1329f997ffe99480db6d3270c228d984934eb04f
Tangil <21197843+TheBlocks@users.noreply.github.com>
2022-05-09 16:05:27 +0100

    Add success alert for user management

--- src/public/scripts/users.js
+++ src/public/scripts/users.js
@@ -1,12 +1,22 @@
 function createExtSpecialist() {
-    sendRequest("/users/create/ext", "extForm", "Unable to create external specialist");
+    sendRequest(
+        "/users/create/ext",
+        "extForm",
+        "Unable to create external specialist",
+        "Successfully created external specialist account"
+    );
 }
 
 function assignTicket() {
-    sendRequest("/ticket/assign", "assignForm", "Unable to assign ticket");
+    sendRequest(
+        "/ticket/assign",
+        "assignForm",
+        "Unable to assign ticket",
+        "Successfully assigned ticket"
+    );
 }
 
-function sendRequest(endpoint, formId, errorMsg) {
+function sendRequest(endpoint, formId, errorMsg, successMsg) {
     let form = document.querySelector("#" + formId);
     let data = new FormData(form);
 
@@ -19,12 +29,16 @@ function sendRequest(endpoint, formId, errorMsg) {
         headers: {"Content-Type": "application/json"},
         body: json
     }).then(res => {
-        if (res.status === 200)
-            location.reload();
-        else {
-            let error = document.querySelector("#error");
+        let error = document.querySelector("#error");
+        let success = document.querySelector("#success");
+        if (res.status === 200) {
+            success.innerHTML = successMsg;
+            success.classList.remove("hide");
+            error.classList.add("hide");
+        } else {
             error.innerHTML = errorMsg;
             error.classList.remove("hide");
+            success.classList.add("hide");
         }
     });
 }
